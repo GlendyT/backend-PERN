ea9a0adfad3a04b94b7da79788d8c463fe8f4c1f
GlendyT <glendytuyuc@gmail.com>
2024-05-19 18:11:53 -0600

    iniciar testing e instalar supertest y server

--- src/server.ts
+++ src/server.ts
@@ -8,10 +8,10 @@ async function connectDB() {
     try {
         await db.authenticate()
         db.sync()
-        console.log(colors.magenta.bold("Conezion exitosa a la BD"))
+      //  console.log(colors.magenta.bold("Conezion exitosa a la BD"))
     } catch (error) {
         //console.log(error)
-        console.log(colors.red.bold ("Hubo un error al conectar a la base de datos"))
+       // console.log(colors.red.bold ("Hubo un error al conectar a la base de datos"))
     }
 }
 connectDB()
@@ -23,6 +23,10 @@ const server = express()
 server.use(express.json())
 
 server.use("/api/products", router)
+
+server.get("/api", (req, res) => {
+    res.json({msg: "Desde Api"})
+})
 
 
 export default server
